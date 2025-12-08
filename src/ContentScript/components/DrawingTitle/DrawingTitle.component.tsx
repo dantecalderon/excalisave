16b9ad9793e8034447dee579582f60cf2814fc30
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2025-12-08 14:31:41 -0500

    feat: enhance Excalidraw client UI with new options and styles
    - Added `appendChild` option to `mountReactComponent` for flexible mounting.
    - Updated `DrawingTitle` component to include a new class and modified title text.
    - Introduced SCSS styles for the `Excalidraw__title` class.

--- src/ContentScript/components/DrawingTitle/DrawingTitle.component.tsx
+++ src/ContentScript/components/DrawingTitle/DrawingTitle.component.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Heading } from "@radix-ui/themes";
+import "./DrawingTitle.styles.scss";
 
 export function DrawingTitle() {
   return (
     <div
+      className="Excalidraw__title"
       style={{
         position: "absolute",
         left: "100px",
       }}
     >
-      <Heading size="1">Nombre del Drawing</Heading>
+      <Heading size="1">Nombre del Drawing2222</Heading>
     </div>
   );
 }
